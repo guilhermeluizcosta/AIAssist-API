5c9512a1f4f4b4583f1130ab6f8de5479234b950
Guilherme LIC <guilhermelc10@gmail.com>
2025-05-26 12:56:03 -0300

    Primeira versão

--- app.py
+++ app.py
@@ -3,22 +3,45 @@ from decouple import config
 from fastapi import FastAPI
 from langchain_groq import ChatGroq
 from langserve import add_routes
+from chains import resumidor_chain, explicar_codigo_chain
 
 os.environ['GROQ_API_KEY'] = config('GROQ_API_KEY')
 
+# Inicializa o modelo
 model = ChatGroq(model="llama-3.3-70b-versatile")
 
+# Inicializa a aplicação FastAPI
 app= FastAPI(
     title= "LlamaServe AI",
     version='0.1',
     description='API de Inteligência Artificial com FastAPI + LangChain + Groq',
 )
 
+# Inicializa as chains
+resumo_chain = resumidor_chain(model)
+explicar_chain = explicar_codigo_chain(model)
+
+
+# Endpoints
 add_routes(
     app,
     model,
     path='/groq',
 )
+
+
+add_routes(
+    app,
+    resumo_chain,
+    path='/resumir',
+)
+
+add_routes(
+    app,
+    explicar_chain,
+    path='/explicar',
+)
+
 
 if __name__ == '__main__':
     import uvicorn
---
+++ chains.py
@@ -0,0 +1,42 @@
+import os
+
+from decouple import config
+
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import ChatPromptTemplate
+
+
+os.environ['GROQ_API_KEY'] = config('GROQ_API_KEY')
+
+def resumidor_chain(model):
+
+    prompt = ChatPromptTemplate.from_template(
+        """
+        Você é um assistente especialista em resumo de textos.
+        Resuma o texto a seguir de forma clara, objetiva e mantendo as informações principais:
+
+        Texto:
+        {texto}
+
+        Resumo:
+        """
+    )
+    chain = prompt | model | StrOutputParser()
+
+    return chain
+
+def explicar_codigo_chain(model):
+    prompt = ChatPromptTemplate.from_template(
+        """
+        Você é um assistente de programação. Receberá um código e deve explicar de forma clara e didática
+        o que ele faz, linha por linha ou por blocos se fizer mais sentido.
+
+        Código:
+        {codigo}
+
+        Explique:
+        """
+    )
+
+    chain = prompt | model | StrOutputParser()
+    return chain
